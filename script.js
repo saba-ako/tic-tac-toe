514672378d570e58673a2009fc89d33767747b33
saba-ako <sabaakobia81@gmail.com>
2024-01-29 03:26:12 +0400

    save

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 let box = document.getElementsByClassName("box")[0];
+let turn = true; 
 let array = [
-    [], [], [], [], [], [], [], [],
+    false, false, false, false, false, false, false, false, false
 ];
-console.log(box);
+
+// let winningCombinations = [
+//     {
+//         [0, 1, 2]
+//         [0, 3, 6]
+//         [0, 4, 8]
+//         [1, 4, 7]
+//         [2, 4, 6]
+//         [2, 5, 8]
+//         [3, 4, 5]
+//         [6, 7, 8]
+//     }
+// ]
+
 array.map((item, index) => {
     let one = document.createElement("div");
-    one.addEventListener("click", () => onclick(index))
     box.appendChild(one);
+    one.addEventListener("click", () => onclick(index, one));
+
     if (index === 0) {
         one.className = "up";
     }
@@ -31,13 +46,28 @@ array.map((item, index) => {
     if (index === 7) {
         one.className = "down";
     }
-}
-)
-
-function onclick(index) {
-    if () {
-        
-    }else {
-
+    if (index === 8) {
+        one.className = "nothing";
     }
+});
+
+function onclick(index, one) {
+    turn = !turn;
+
+    if (!array[index]) {
+        if (turn) {
+            let image = document.createElement("img");
+            image.src = "/o.png";
+            image.className = "o";
+            one.appendChild(image);
+        } else {
+            let image = document.createElement("img");
+            image.src = "/x.png";
+            image.className = "o"
+            one.appendChild(image);   
+        }
+
+        array[index] = true;
+    }    
 }
+
